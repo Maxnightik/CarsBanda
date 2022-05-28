a672451c375451b35036fcd46ca91a4beff0ef7f
Maxnightik <98279735+Maxnightik@users.noreply.github.com>
2022-05-28 09:52:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ audioPlayer = document.querySelector("audio");
 source = document.querySelector("audio source");
 // машинка
 car = document.querySelector("#car");
-console.dir(car);
+// машинка врага
+enemyCar = document.querySelector("#enemycar");
+
 // начало игры
 start.onclick = function () {
   // показать блок игры
@@ -22,31 +24,84 @@ start.onclick = function () {
   start.style.display = "none";
   // запустить музыку
   audioPlayer.play();
+  //createLine1();
+  //створення доріжки 
+  //createLine2();
+  cyclecreateLine();
+  
 };
 
-// управление
+// управление машинкой
 document.onkeydown = function (event) {
-  // если нажали w
-  if (event.keyCode == 87) {
-    // если позиция игрока больше 50px
-    if (car.offsetTop > 50)
+  console.dir(event)
+    // если нажали вверх
+  if (event.keyCode == 87)
+   {
+    if (car.offsetTop > 50){
+      // если позиция игрока больше 50px
       // то двигаться вверх
-      car.style.top = car.offsetTop - 20 + "px";
-  } // если нажали s
-  if (event.keyCode == 83) {
-    // если позиция игрока меньше 640px
-    if (car.offsetTop < 640)
+      car.style.top = car.offsetTop - 10 + "px";
+    }
+  }
+  if (event.keyCode == 83) 
+  {
+    
+      // если позиция игрока меньше 640px
       // то двигаться вниз
-      car.style.top = car.offsetTop + 20 + "px";
+      car.style.top = car.offsetTop + 10 + "px";
   }
-  // если нажали a
-  if (event.keyCode == 68) {
+  if (event.keyCode == 65 ) {
+     // если нажали  влево
     // то двигаться влево
-    car.style.left = car.offsetLeft + 20 + "px";
+    car.style.left = car.offsetLeft - 10 + "px";
   }
-  // если нажали d
-  if (event.keyCode == 65) {
+  // если нажали вправо
+  if (event.keyCode == 68) {
     // то двигаться вправо
-    car.style.left = car.offsetLeft - 20 + "px";
+    car.style.left = car.offsetLeft + 10 + "px";
   }
+  console.dir(car)
 };
+
+
+
+// createLine();
+function createLine1(){
+  let lineLong  = document.createElement("div");
+  lineLong.className ="line1";
+  main.appendChild(lineLong);
+  moveLine(lineLong);
+}
+function createLine2(){
+  let lineShort  = document.createElement("div");
+  lineShort.className ="line2";
+  main.appendChild(lineShort);
+  moveLine(lineShort);
+}
+function moveLine(line){
+  timerId = setInterval(function (){
+    line.style.top =  line.offsetTop + 3 + "px";
+      if(line.offsetTop >880){
+        line.remove();
+        
+      }
+    
+  },5)
+}
+
+function cyclecreateLine(){
+  let allline = main.querySelectorAll('line1').length;
+  console.dir(allline);
+  if (allline<2){
+    setInterval(function(){
+      createLine1();
+    },500)  
+  }
+  
+  
+  
+}
+
+
+
+
